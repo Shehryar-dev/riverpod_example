867460f6d5a87159f1475cf193eb0a06addd1955
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-05 02:42:05 +0500

    Riverpod Example

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:riverpod_example/home/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RiverPod',
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
