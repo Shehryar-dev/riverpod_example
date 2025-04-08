1393dacacb6696e5271483dcd408e085d9be699e
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-09 04:26:12 +0500

    Riverpod Example

---
+++ lib/examples/text_field_example/text_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.switch_left),
+            SizedBox(width: 11),
+            Text('Stats Notifier Provider'),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
