0d7cf1908d8b923397f6c208d2f2d80cb08261ee
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-09 13:30:35 +0500

    Riverpod Example

---
+++ lib/examples/text_field_example/provider/textfield_provider.dart
@@ -0,0 +1,26 @@
+import 'package:riverpod/riverpod.dart';
+
+
+final textFieldProvider = StateNotifierProvider<TextFieldNotifier, TextFieldState>((ref){
+    return TextFieldNotifier();
+});
+
+
+class TextFieldNotifier extends StateNotifier<TextFieldState>{
+  TextFieldNotifier() : super(TextFieldState(controller: '', isVisible: false));
+
+  void search(String query){
+    state = state.copyWith(controller: query);
+  }
+}
+
+class TextFieldState{
+  final String controller;
+  final bool isVisible;
+  const TextFieldState({required this.controller, required this.isVisible});
+
+  TextFieldState copyWith({String? controller, bool? isVisible}){
+    return TextFieldState(controller: controller ?? this.controller, isVisible: isVisible ?? this.isVisible );
+  }
+
+}
