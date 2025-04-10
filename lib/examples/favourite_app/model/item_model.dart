27ba03a8c77f9ad1c15ddbd9fe6ad52faae73da6
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-11 03:08:22 +0500

    Riverpod Example

---
+++ lib/examples/favourite_app/model/item_model.dart
@@ -0,0 +1,12 @@
+class Item{
+  final String id;
+  final String name;
+  final bool isFavourite;
+
+  const Item({ required this.id, required this.name, required this.isFavourite});
+
+  Item copyWith({String? id, String? name, bool? isFavourite}){
+    return Item(id: id ?? this.id, name: name ?? this.name, isFavourite: isFavourite ?? this.isFavourite);
+  }
+
+}
